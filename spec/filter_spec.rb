2ff85fd56b07283077b9f2011ac7bc7ee84a0a38
Dana Scheider <dana.scheider@gmail.com>
2014-08-22 13:43:57 -0700

    Example for content of @conditions

--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -3,9 +3,19 @@ require 'spec_helper'
 describe ActiveFilter::Filter do 
   include ActiveFilter
 
-  let(:filter) { ActiveFilter::Filter.new(foo: 'bar') }
+  let(:filter) { ActiveFilter::Filter.new(all: { foo: 'bar' }) }
 
-  it 'initializes with a set of conditions' do 
-    expect(filter.instance_variables).to include(:@conditions) 
+  describe '::initialize' do 
+    it 'stores conditions in an instance variable' do 
+      expect(filter.instance_variables).to include(:@conditions) 
+    end
+  end
+
+  describe 'instance methods' do 
+    describe '#conditions' do 
+      it 'returns conditions with which it was initialized' do 
+        expect(filter.conditions).to eql({all: {foo: 'bar'}})
+      end
+    end
   end
 end
