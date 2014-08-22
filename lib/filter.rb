65694461b8c0fd54b696b9f7698314b192b1e3f0
Dana Scheider <dana.scheider@gmail.com>
2014-08-22 12:33:32 -0700

    Add comments

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,4 +1,9 @@
 module ActiveFilter
+
+  # ActiveFilter::Filter is a base class used to generate filters for ActiveRecord
+  # resources. Its subclasses follow the naming scheme <class>Filter, where <class>
+  # is the camel-cased name of an ActiveRecord::Base subclass.
+
   class Filter
     attr_reader :conditions
     
