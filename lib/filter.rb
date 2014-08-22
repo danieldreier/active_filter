13146c0fa236ad5c8b6259f7490a69b1c3a328e2
Dana Scheider <dana.scheider@gmail.com>
2014-08-22 12:44:43 -0700

    Improve documentation

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,7 +1,7 @@
 module ActiveFilter
 
   # ActiveFilter::Filter is a base class used to generate filters for ActiveRecord
-  # resources. Its subclasses follow the naming scheme <class>Filter, where <class>
+  # resources. Its subclasses follow the naming scheme ++<class>Filter++, where <class>
   # is the camel-cased name of an ActiveRecord::Base subclass.
 
   class Filter
@@ -10,7 +10,16 @@ module ActiveFilter
     # The conditions used in the initialize method will probably end up being
     # removed; instead, subclasses will be initialized with conditions through the larger
     # ActiveFilter module.
-    
+
+    # Conditions take the form of a hash. Hash keys may include ++:all++, ++:any++, and ++:none++,
+    # each of which points to a hash of attributes for the ActiveRecord model being filtered.
+    # The ++:all++, ++:any++, and ++:none++ keys function as logical AND, OR, and NOT operators, and may be
+    # nested. For example, given a model ++Task++ with attributes ++:priority++, ++status++, and 
+    # ++category++, a ++TaskFilter++ could be initialized with the following conditions:
+    #   ++{all: { priority: ['urgent', 'high'], status: 'incomplete' }}++
+
+    # The filter would then return all incomplete tasks whose status was either 'urgent' or 'high'.
+
     def initialize(conditions)
       @conditions = conditions
     end
