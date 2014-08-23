e34b518bd7e26a0777dc01bb6342263c7b1964d9
Dana Scheider <dana.scheider@gmail.com>
2014-08-23 13:38:28 -0700

    Comments listing models' attributes

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,13 +1,19 @@
 require 'active_record'
 
+# User attributes are :username, :birthdate, :is_admin (default false), :created_at, :updated_at
+
 class User < ActiveRecord::Base
   has_one :list
 end
+
+# List attributes are :name, :description, :user_id, :created_at, :updated_at
 
 class List < ActiveRecord::Base 
   belongs_to :user
   has_many :tasks
 end
+
+# Task attributes are :title, :status, :list_id, :deadline, :created_at, :updated_at
 
 class Task < ActiveRecord::Base
   belongs_to :list
