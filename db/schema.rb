94e7b4be9c4b750f35805cff1003280ac108c255
Dana Scheider <dana.scheider@gmail.com>
2014-08-23 13:50:48 -0700

    Add user_id column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822175156) do
+ActiveRecord::Schema.define(version: 20140823204453) do
 
   create_table "lists", force: true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140822175156) do
     t.datetime "deadline"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
